b752cd5002f519858ea72c942eac9467c4d77cf3
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-06-23 18:41:48 -0300

    creacion de proyecto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DolarHomeComponent } from './dolar-home/dolar-home.component';
+import { DolarConsultarComponent } from './dolar-consultar/dolar-consultar.component';
+import { DolarConvertirComponent } from './dolar-convertir/dolar-convertir.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DolarHomeComponent },
+  { path: 'consultar', component: DolarConsultarComponent },
+  { path: 'convertir', component: DolarConvertirComponent }
+];
---
+++ src/app/dolar-home/dolar-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dolar-home',
+  imports: [RouterModule],
+  templateUrl: './dolar-home.component.html',
+  styleUrl: './dolar-home.component.css'
+})
+export class DolarHomeComponent {
+
+}

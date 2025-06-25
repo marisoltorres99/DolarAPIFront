e2b18bd85d7864e07d12734fdd047e0210d5337b
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-06-25 12:28:29 -0300

    implementacion del componente dolar-convertir

--- src/app/dolar-convertir/dolar-convertir.component.ts
+++ src/app/dolar-convertir/dolar-convertir.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dolar-convertir',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './dolar-convertir.component.html',
   styleUrl: './dolar-convertir.component.css'
 })
 export class DolarConvertirComponent {
+  tipoDolar: string = '';
+  cantidad: number | null = null;
+  resultado: number | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  convertir() {
+    if (!this.tipoDolar || this.cantidad == null) return;
+
+    const url = `http://localhost:5016/api/dolar/${this.tipoDolar}/${this.cantidad}`;
+
+    this.http.get(url, { responseType: 'text' }).subscribe({
+      next: (data) => {
+        this.resultado = parseFloat(data);
+      },
+      error: (err) => {
+        console.error('Error al convertir:', err);
+        this.resultado = null;
+      }
+    });
+  }
 }
